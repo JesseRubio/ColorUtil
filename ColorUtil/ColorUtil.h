76a3fe143db77e8d135c8ec9934dedcaf2671cc7
Jesse Rubio <jesserubio@jesses-mbp.attlocal.net>
2019-07-24 17:24:13 -0500

    First commit

---
+++ ColorUtil/ColorUtil.h
@@ -0,0 +1,19 @@
+//
+//  ColorUtil.h
+//  ColorUtil
+//
+//  Created by Jesse Rubio on 7/24/19.
+//  Copyright © 2019 insomniacdvlpr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorUtil.
+FOUNDATION_EXPORT double ColorUtilVersionNumber;
+
+//! Project version string for ColorUtil.
+FOUNDATION_EXPORT const unsigned char ColorUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorUtil/PublicHeader.h>
+
+
